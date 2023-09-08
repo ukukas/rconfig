154e50a789c0cb93758abc5ae979ed5e894dcd1e
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2023-09-08 12:48:50 -0400

    fix configuration file

--- configure-rstudio.ps1
+++ configure-rstudio.ps1
@@ -4,22 +4,22 @@ $registryKey = "HKLM:\SOFTWARE\R-core\R"
 $installPath = (Get-ItemProperty $registryKey -Name "InstallPath").InstallPath
 $exePath = (Join-Path $installPath "bin\x64\Rterm.exe").Replace("\", "\\")
 
-$settingsContent = @"
+$configContent = @"
 {
     "platform": {
 		"windows": {
 			"rBinDir": "",
 			"preferR64": true,
-			"rExecutablePath": $exePath"
+			"rExecutablePath": "$exePath"
 		}
-	},
+	}
 }
 "@
 
 Get-ChildItem (Join-Path $env:SystemDrive "Users") -Force -Directory `
 -Exclude "All Users","Default User","Public" | ForEach-Object {
-    $settingsDir = Join-Path $_.FullName "AppData\Roaming\RStudio"
-    New-Item -ItemType Directory -Force -Path $settingsDir
-    $settingsFile = Join-Path $settingsDir "settings.json"
-    New-Item -ItemType File -Force -Path $settingsFile -Value $settingsContent
+    $configDir = Join-Path $_.FullName "AppData\Roaming\RStudio"
+    New-Item -ItemType Directory -Force -Path $configDir
+    $configFile = Join-Path $configDir "config.json"
+    New-Item -ItemType File -Force -Path $configFile -Value $configContent
 }
